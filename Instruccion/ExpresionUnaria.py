a052e6743b5635bf9dda2c28784512b1f4850017
Javier Aguilar <josuejaviera19@gmail.com>
2023-06-18 19:51:05 -0600

    Expresiones Unarias 3D

--- Instruccion/ExpresionUnaria.py
+++ Instruccion/ExpresionUnaria.py
@@ -2,6 +2,7 @@ from Instruccion.Instruccion import instruccion
 from Tipos.Tipos import *
 from Ejecucion.Valor import valor
 from Ejecucion.Entorno import entorno
+from C3D.Valor3D import valor3D
 
 class expresionUnaria(instruccion):
     '''
@@ -248,4 +249,75 @@ class expresionUnaria(instruccion):
             return retorno
 
     def c3d(self, SIMBOLOS, REPORTES, CODIGO):
-        pass
+        #Obtener el valor de las expresiones
+        expresion = self.expresion.c3d(SIMBOLOS, REPORTES, CODIGO)
+
+        #Comprobar que sea primitivo
+        if expresion.clase != Clases.PRIMITIVO.value:
+            CODIGO.insertar_Comentario("ERROR: El operador izquierdo no es de tipo Primitivo")
+            REPORTES.salida += "ERROR: El operador izquierdo no es de tipo Primitivo. \n"
+            mensaje = "El operador izquierdo no es de tipo Primitivo."
+            REPORTES.añadirError("Semantico", mensaje, self.linea, self.columna)
+
+            temporal = CODIGO.nuevoTemporal()
+            CODIGO.insertar_Asignacion(temporal, "0")
+            return valor3D(temporal, True, Tipo.NUMBER.value, Clases.PRIMITIVO.value)
+        
+        #Separacion de instruccion
+        if self.tipoOperacion == Expresion.NOT.value:
+            #Evaluar el caso en que ambos sean boolean
+            if expresion.tipo == Tipo.BOOLEAN.value:
+                #Crear el temporal. Resulatdo almacena el resultado de la comparacion. 1 en el true, 0 en el false.
+                #Se le asigna 0 de una vez para que vaya al label de salida si no cumple
+                tempResultado = CODIGO.nuevoTemporal()
+                CODIGO.insertar_Asignacion(tempResultado, "0")
+
+                #Declarar lables de entrada y salida
+                labelVerdadero = CODIGO.nuevoLabel()
+                labelSalida = CODIGO.nuevoLabel()
+
+                #---Como se inicializa en 0, si es verdadero el valor de salida ya va a tener 0
+                CODIGO.insertar_If(expresion.valor, "==", "1", labelSalida)
+                CODIGO.insertar_Goto(labelVerdadero)
+
+                #--Si es 0 entra aca y cambia a 1
+                CODIGO.insertar_Label(labelVerdadero)
+                CODIGO.insertar_Asignacion(tempResultado, "1")
+
+                #-- La condicion es falsa o acabo la verdadera
+                CODIGO.insertar_Label(labelSalida)
+                return valor3D(tempResultado, True, Tipo.BOOLEAN.value, Clases.PRIMITIVO.value)
+                
+            else:
+                REPORTES.salida += "ERROR: La operación not solo recibe booleanos. \n"
+                mensaje = "La operación not solo recibe booleanos."
+                REPORTES.añadirError("Semantico", mensaje, self.linea, self.columna)
+                CODIGO.insertar_Comentario("ERROR: La operación not solo recibe booleanos.")
+
+                temporal = CODIGO.nuevoTemporal()
+                CODIGO.insertar_Asignacion(temporal, "0")
+                return valor3D(temporal, True, Tipo.NUMBER.value, Clases.PRIMITIVO.value)
+        
+        elif self.tipoOperacion == Expresion.UNARIO.value:
+            CODIGO.insertar_Comentario("////////// INICIA RESTA //////////")
+            #Evaluar el caso en que ambos sean number
+            if expresion.tipo == Tipo.NUMBER.value:
+                #Crear el temporal y añadirle la resta de los temporales
+                tempResultado = CODIGO.nuevoTemporal()
+                CODIGO.insertar_Expresion(tempResultado, "0", "-", expresion.valor)
+
+                return valor3D(tempResultado, True, Tipo.NUMBER.value, Clases.PRIMITIVO.value)
+    
+            else:
+                CODIGO.insertar_Comentario("ERROR: La negacion unaria solo recibe numbers.")
+                REPORTES.salida += "ERROR: La negacion unaria solo recibe numbers. \n"
+                mensaje = "La negacion unaria solo recibe numbers."
+                REPORTES.añadirError("Semantico", mensaje, self.linea, self.columna)
+
+                temporal = CODIGO.nuevoTemporal()
+                CODIGO.insertar_Asignacion(temporal, "0")
+                return valor3D(temporal, True, Tipo.NUMBER.value, Clases.PRIMITIVO.value)
+            
+        
+        
+        

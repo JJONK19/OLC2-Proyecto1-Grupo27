75a8df0b3b8d8202d7229b970199f1a4f0449191
02JuanJo <61897534+02JuanJo@users.noreply.github.com>
2023-06-26 04:26:18 -0600

    Pushito Final Final

--- Analizador.py
+++ Analizador.py
@@ -230,14 +230,7 @@ class Analizador:
 
 
     # Error
-    '''
-    #Por si no jala el otro
-    def t_error(t):
-        print(Analizador.arbol.reporte)
-        mensaje = f'Caracter no reconocido {t.value[0]!r}.'
-        Analizador.arbol.añadirError("Lexico", mensaje, t.lexer.lineno, 0)
-        t.lexer.skip(1)
-    '''
+
 
     def t_error(t):
         mensaje = f'Caracter no reconocido {t.value[0]!r}.'
